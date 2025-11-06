6388a434d04aa61cc0925844ddf68e1fa9965113
Pelumi <imulep2104@gmail.com>
2025-11-06 21:48:43 +0100

    Complete Part 1 exercises 1.61.14

---
+++ Part 1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,35 @@
+import StatisticLine from './StatisticLine'
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average.toFixed(1)} />
+          <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Statistics
